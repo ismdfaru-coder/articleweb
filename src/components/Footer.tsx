1e13cca2d7cdd5dc3bd8b3fd9c1c05cd98ffed8d
mohammed farhathulla <ismdfaru@gmail.com>
2025-10-31 23:37:05 +0000

    remove admin link from the front page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,10 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="border-t">
-      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 py-8 md:flex-row">
+      <div className="container mx-auto flex flex-col items-center justify-center gap-4 px-4 py-8 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
           &copy; {new Date().getFullYear()} Life Reality Insights. All Rights Reserved.
         </p>
-        <div className="text-sm text-muted-foreground">
-          <Link href="/login" className="hover:text-foreground">
-            Admin
-          </Link>
-        </div>
       </div>
     </footer>
   );
